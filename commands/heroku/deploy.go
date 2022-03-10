5a3f2f022e5490873cdbafd38494c566bfd6922a
Kevin C. Krinke <kevin@krinke.ca>
2022-03-10 11:27:57 -0500

    commands/heroku: implement deploy-slug command

--- commands/heroku/deploy.go
+++ commands/heroku/deploy.go
@@ -1,57 +1,200 @@
 package heroku
 
 import (
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/urfave/cli/v2"
 
+	"github.com/go-enjin/be/pkg/cli/git"
+	bePath "github.com/go-enjin/be/pkg/path"
+	"github.com/go-enjin/enjenv/pkg/basepath"
 	"github.com/go-enjin/enjenv/pkg/io"
+	"github.com/go-enjin/enjenv/pkg/system"
 )
 
-func (s *Command) makeDeployCommand(appNamePrefix string) *cli.Command {
+func (c *Command) makeDeploySlugCommand(appNamePrefix string) *cli.Command {
 	return &cli.Command{
-		Name:      "deploy",
-		Category:  s.TagName,
-		Usage:     "deploy a git repository in a heroku fashion to a local path",
-		UsageText: appNamePrefix + " deploy <src> <dst> [options]",
+		Name:      "deploy-slug",
+		Category:  c.TagName,
+		Usage:     "deploy a git repository in a heroku slug fashion to a local path",
+		UsageText: appNamePrefix + " deploy-slug <src> <dst> [options]",
 		Description: `
-deploy will perform the following steps, stopping on the first error:
+deploy-slug will perform the following steps, stopping on any error:
 
-	- rename the destination path (if it exists)
-	- clone the git repo into the destination path
-	- configure the shell environment variables for further sub-commands
-	- check if a Slugfile is present, error otherwise
-	- initialize a local enjin environment, includes nodejs if package.json detected
-	- if a Makefile is detected:
-		- check for one target: release
-		- run 'make release'
-	- if no Makefile is present:
-		- run 'enjenv golang build --verbose'
-		- run 'enjenv finalize-slug --verbose --force'
-
-See: the run-deployed command for next steps.
+	- if <dst> exists:
+		- if --backup:
+			- rename <dst> to <backup-path>
+		- if not --no-backup:
+			- rename <dst> to <dst>--backup--YYYYMMDD-HHMMSS-ZONE
+	- else:
+		- mkdir <dst> and cd <dst>
+	- run git clone <src> .
+	- run enjenv init --golang "--golang" --nodejs "--nodejs"
+	- run make "--release-targets"
+	- run enjenv clean --force
+	- run rm -rf .git
+	- run enjenv finalize-slug --verbose --force
 `,
 		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "backup",
+				Usage:   "specify the backup directory path",
+				EnvVars: []string{system.EnvPrefix + "_DEPLOY_SLUG_BACKUP"},
+			},
 			&cli.BoolFlag{
-				Name:    "verbose",
-				Usage:   "output detailed information",
-				Aliases: []string{"v"},
+				Name:    "no-backup",
+				Usage:   "if <dst> exists, remove it without backing up",
+				EnvVars: []string{system.EnvPrefix + "_DEPLOY_SLUG_NO_BACKUP"},
+			},
+			&cli.StringSliceFlag{
+				Name:    "target",
+				Usage:   "specify one or more build targets to use",
+				Value:   cli.NewStringSlice("release"),
+				EnvVars: []string{system.EnvPrefix + "_DEPLOY_SLUG_TARGETS"},
+				Aliases: []string{"t"},
 			},
 			&cli.StringFlag{
-				Name:  "target",
-				Usage: "specify the target name to use",
-				Value: "release",
+				Name:    "golang",
+				Usage:   "pass through to 'golang init --golang'",
+				EnvVars: []string{system.EnvPrefix + "_DEPLOY_SLUG_GOLANG"},
 			},
+			&cli.StringFlag{
+				Name:    "nodejs",
+				Usage:   "pass through to 'nodejs init --nodejs'",
+				EnvVars: []string{system.EnvPrefix + "_DEPLOY_SLUG_NODEJS"},
+			},
+			// &cli.BoolFlag{
+			// 	Name:    "verbose",
+			// 	Usage:   "output detailed information",
+			// 	Aliases: []string{"v"},
+			// 	EnvVars: []string{system.EnvPrefix+"_DEPLOY_SLUG_VERBOSE"},
+			// },
 		},
-		Action: func(ctx *cli.Context) (err error) {
-			if err = s.Prepare(ctx); err != nil {
-				return
-			}
-			argv := ctx.Args().Slice()
-			if len(argv) == 0 {
-				cli.ShowCommandHelpAndExit(ctx, "write-slugfile", 1)
-				return
-			}
-			io.StderrF("not implemented")
-			return
-		},
+		Action: c.ActionDeploySlug,
 	}
+}
+
+func (c *Command) ActionDeploySlug(ctx *cli.Context) (err error) {
+	if err = c.Prepare(ctx); err != nil {
+		return
+	}
+	argv := ctx.Args().Slice()
+	if len(argv) < 2 {
+		cli.ShowCommandHelpAndExit(ctx, "deploy-slug", 1)
+		return
+	}
+	src := argv[0]
+	dst := argv[1]
+
+	_ = os.Setenv(system.EnvPrefix+"_NOTIFY_PREFIX", "deploy-slug "+bePath.Base(src))
+	io.NotifyF("deploy-slug", "starting local deployment")
+	io.StdoutF("# src: %v, dst: %v\n", src, dst)
+
+	if bePath.IsFile(dst) {
+		err = fmt.Errorf("<dst> is a file, something must be wrong")
+		return
+	}
+
+	if bePath.IsDir(dst) {
+		switch {
+		case ctx.Bool("no-backup"):
+			io.StdoutF("# removing %v, --no-backup\n", dst)
+			if err = os.RemoveAll(dst); err != nil {
+				return
+			}
+		default:
+			var backupPath string
+			if backupPath = ctx.String("backup"); backupPath == "" {
+				now := time.Now()
+				backupPath = fmt.Sprintf(
+					"%v/%v--backup--%v",
+					bePath.Dir(dst),
+					bePath.Base(dst),
+					now.Format("20060102-150405-0700"),
+				)
+			}
+			io.StdoutF("# renaming %v => %v\n", dst, backupPath)
+			if err = os.Rename(dst, backupPath); err != nil {
+				return
+			}
+		}
+	}
+
+	io.StdoutF("# preparing destination\n")
+	if err = os.Mkdir(dst, 0770); err != nil {
+		err = fmt.Errorf("mkdir error: %v", err)
+		return
+	}
+	pwd := bePath.Pwd()
+	if err = os.Chdir(dst); err != nil {
+		err = fmt.Errorf("chdir %v error: %v", dst, err)
+		return
+	}
+
+	if _, err = git.Exe("clone", src, "."); err != nil {
+		err = fmt.Errorf("git clone %v . error: %v", src, err)
+		return
+	}
+
+	// 	- run enjenv init --golang "--golang" --nodejs "--nodejs"
+	io.StdoutF("# initializing enjenv\n")
+
+	if basepath.EnjenvPresent() {
+		io.StdoutF("# enjenv path present, masking for local\n")
+		_ = os.Setenv("ENVJENV_PATH", "")
+	}
+	// basepath.EnjenvPath = basepath.FindEnjenvDir()
+	// _ = os.Setenv("ENVJENV_PATH", basepath.EnjenvPath)
+
+	var initArgs []string
+	initArgs = append(initArgs, "init")
+	if golang := ctx.String("golang"); golang != "" {
+		initArgs = append(initArgs, "--golang", golang)
+	}
+	if nodejs := ctx.String("nodejs"); nodejs != "" {
+		initArgs = append(initArgs, "--nodejs", nodejs)
+	}
+	initArgs = append(initArgs, ".")
+
+	if _, err = c.enjenvExe(initArgs...); err != nil {
+		err = fmt.Errorf("enjenv init error: %v", err)
+		return
+	}
+
+	//	- run make "--release-targets"
+	targets := ctx.StringSlice("release-targets")
+	io.NotifyF("deploy-slug", "making release targets: %v\n", targets)
+	if _, err = c.makeExe(targets...); err != nil {
+		err = fmt.Errorf("make error: %v", err)
+		return
+	}
+	io.NotifyF("deploy-slug", "release targets completed: %v\n", targets)
+
+	//	- run enjenv clean --force
+	io.StdoutF("# enjenv cleaning up\n")
+	if _, err = c.enjenvExe("clean", "--force"); err != nil {
+		err = fmt.Errorf("enjenv clean error: %v", err)
+		return
+	}
+
+	if err = os.RemoveAll(".git"); err != nil {
+		err = fmt.Errorf("removing .git error: %v", err)
+		return
+	}
+
+	//	- run enjenv finalize-slug --verbose --force
+	io.NotifyF("deploy-slug", "enjenv finalize-slug")
+	if _, err = c.enjenvExe("finalize-slug", "--verbose", "--force"); err != nil {
+		err = fmt.Errorf("enjenv finalize-slug error: %v", err)
+		return
+	}
+
+	if err = os.Chdir(pwd); err != nil {
+		err = fmt.Errorf("chdir %v error: %v", pwd, err)
+		return
+	}
+	io.NotifyF("deploy-slug", "local deployment completed")
+	return
 }
