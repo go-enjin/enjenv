c9e46f56c307b055403d99288365855e079a41b0
Kevin C. Krinke <kevin@krinke.ca>
2022-03-12 12:15:09 -0500

    pkg/run: add AddPathToenv and RemovePathFromEnv helpers

--- pkg/run/run.go
+++ pkg/run/run.go
@@ -17,10 +17,22 @@ package run
 import (
 	"fmt"
 
+	"github.com/go-enjin/be/pkg/cli/env"
 	"github.com/go-enjin/be/pkg/cli/run"
 
 	"github.com/go-enjin/enjenv/pkg/basepath"
 )
+
+func AddPathToEnv(path string) {
+	_ = env.SetPathRemoved(path)
+	_ = env.SetPathPrefixed(path)
+	return
+}
+
+func RemovePathFromEnv(path string) {
+	_ = env.SetPathRemoved(path)
+	return
+}
 
 func MakeExe(argv ...string) (err error) {
 	err = run.CheckExe("make", argv...)
