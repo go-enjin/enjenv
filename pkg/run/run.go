d6b6afbe8e55cdbd07ddb360aff967522e78de6e
Kevin C. Krinke <kevin@krinke.ca>
2022-03-12 11:29:45 -0500

    pkg/run: Exe wrappers for make and enjenv binaries

---
+++ pkg/run/run.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2022  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package run
+
+import (
+	"fmt"
+
+	"github.com/go-enjin/be/pkg/cli/run"
+
+	"github.com/go-enjin/enjenv/pkg/basepath"
+)
+
+func MakeExe(argv ...string) (err error) {
+	err = run.CheckExe("make", argv...)
+	return
+}
+
+func EnjenvExe(argv ...string) (err error) {
+	if enjenvBin := basepath.WhichBin(); enjenvBin != "" {
+		err = run.CheckExe(enjenvBin, argv...)
+	} else {
+		err = fmt.Errorf("enjenv not found")
+	}
+	return
+}
