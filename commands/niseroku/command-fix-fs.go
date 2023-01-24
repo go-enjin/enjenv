75ef3034f14cdbfeae5ad90fbff1a250973486ed
Kevin C. Krinke <kevin@krinke.ca>
2023-01-24 17:09:18 -0500

    commands/niseroku (fix-fs): migrate to new services; code cleanups and cosmetic changes

--- commands/niseroku/command-fix-fs.go
+++ commands/niseroku/command-fix-fs.go
@@ -18,12 +18,12 @@ import "C"
 import (
 	"fmt"
 	"os"
-	"os/user"
-	"strconv"
 	"syscall"
 
-	bePath "github.com/go-enjin/be/pkg/path"
 	"github.com/urfave/cli/v2"
+
+	bePath "github.com/go-enjin/be/pkg/path"
+	"github.com/go-enjin/enjenv/pkg/service/common"
 
 	beIo "github.com/go-enjin/enjenv/pkg/io"
 )
@@ -38,29 +38,12 @@ func (c *Command) actionFixFs(ctx *cli.Context) (err error) {
 		return
 	}
 
-	var s *Server
-	if s, err = NewServer(c.config); err != nil {
-		return
-	}
-
-	var u *user.User
-	if u, err = user.Lookup(s.Config.RunAs.User); err != nil {
-		return
-	}
-	var g *user.Group
-	if g, err = user.LookupGroup(s.Config.RunAs.Group); err != nil {
-		return
-	}
-
 	var uid, gid int
-	if uid, err = strconv.Atoi(u.Uid); err != nil {
-		return
-	}
-	if gid, err = strconv.Atoi(g.Gid); err != nil {
+	if uid, gid, err = common.GetUidGid(c.config.RunAs.User, c.config.RunAs.Group); err != nil {
 		return
 	}
 
-	for _, p := range []string{s.Config.Paths.Etc, s.Config.Paths.Tmp, s.Config.Paths.Var} {
+	for _, p := range []string{c.config.Paths.Etc, c.config.Paths.Tmp, c.config.Paths.Var} {
 		if err = os.Chown(p, uid, gid); err != nil {
 			beIo.StderrF("error changing ownership of: %v - %v\n", p, err)
 			continue
@@ -82,19 +65,23 @@ func (c *Command) actionFixFs(ctx *cli.Context) (err error) {
 		}
 	}
 
-	if s.Config.LogFile != "" {
-		if !bePath.IsFile(s.Config.LogFile) {
-			if err = os.WriteFile(s.Config.LogFile, []byte(""), 0660); err != nil {
-				beIo.StderrF("error preparing log file: %v - %v\n", s.Config.LogFile, err)
+	if c.config.LogFile != "" {
+		if !bePath.IsFile(c.config.LogFile) {
+			if err = os.WriteFile(c.config.LogFile, []byte(""), 0660); err != nil {
+				beIo.StderrF("error preparing log file: %v - %v\n", c.config.LogFile, err)
 			}
 		}
-		if err = os.Chown(s.Config.LogFile, uid, gid); err != nil {
-			beIo.StderrF("error changing ownership of: %v - %v\n", s.Config.LogFile, err)
+		if err = os.Chown(c.config.LogFile, uid, gid); err != nil {
+			beIo.StderrF("error changing ownership of: %v - %v\n", c.config.LogFile, err)
 		}
 	}
 
-	if err = os.Chown(s.Config.Paths.PidFile, uid, gid); err != nil {
-		beIo.StderrF("error changing ownership of: %v - %v\n", s.Config.Paths.PidFile, err)
+	if err = os.Chown(c.config.Paths.ProxyPidFile, uid, gid); err != nil {
+		beIo.StderrF("error changing ownership of: %v - %v\n", c.config.Paths.ProxyPidFile, err)
+	}
+
+	if err = os.Chown(c.config.Paths.RepoPidFile, uid, gid); err != nil {
+		beIo.StderrF("error changing ownership of: %v - %v\n", c.config.Paths.RepoPidFile, err)
 	}
 
 	beIo.STDOUT("# filesystem repair completed\n")
