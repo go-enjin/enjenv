477a3e19caa3f584faa02994050620ffaca6cd14
Kevin C. Krinke <kevin@krinke.ca>
2023-01-24 16:45:42 -0500

    pkg/service/common: new home for common service funcs

--- pkg/service/common/common.go
+++ pkg/service/common/common.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022  The Go-Enjin Authors
+// Copyright (c) 2023  The Go-Enjin Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,19 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package niseroku
+package common
 
+import "C"
 import (
 	"encoding/csv"
 	"fmt"
 	"net"
+	"os/user"
 	"regexp"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/shirou/gopsutil/v3/process"
 
+	beIo "github.com/go-enjin/enjenv/pkg/io"
 	pkgRun "github.com/go-enjin/enjenv/pkg/run"
 )
 
@@ -32,7 +36,7 @@ var (
 	RxSshPubKey = regexp.MustCompile(`^(\S+)\s+(\S+)((?:\s*).*)$`)
 )
 
-func parseSshKey(input string) (prefix, data, comment, id string, ok bool) {
+func ParseSshKey(input string) (prefix, data, comment, id string, ok bool) {
 	if ok = RxSshPubKey.MatchString(input); ok {
 		m := RxSshPubKey.FindAllStringSubmatch(input, 1)
 		prefix, data, comment = m[0][1], m[0][2], m[0][3]
@@ -41,22 +45,22 @@ func parseSshKey(input string) (prefix, data, comment, id string, ok bool) {
 	return
 }
 
-func parseArgv(input string) (argv []string, err error) {
+func ParseControlArgv(input string) (argv []string, err error) {
 	r := csv.NewReader(strings.NewReader(input))
 	r.Comma = ' '
 	argv, err = r.Read()
 	return
 }
 
-func sendSignalToPidFromFile(pidFile string, sig process.Signal) (err error) {
+func SendSignalToPidFromFile(pidFile string, sig process.Signal) (err error) {
 	var proc *process.Process
-	if proc, err = getProcessFromPidFile(pidFile); err == nil {
+	if proc, err = GetProcessFromPidFile(pidFile); err == nil {
 		err = proc.SendSignal(sig)
 	}
 	return
 }
 
-func getProcessFromPid(pid int) (proc *process.Process, err error) {
+func GetProcessFromPid(pid int) (proc *process.Process, err error) {
 	var running bool
 	if proc, err = process.NewProcess(int32(pid)); err != nil {
 		err = fmt.Errorf("process not found")
@@ -71,23 +75,56 @@ func getProcessFromPid(pid int) (proc *process.Process, err error) {
 	return
 }
 
-func getProcessFromPidFile(pidFile string) (proc *process.Process, err error) {
+func GetProcessFromPidFile(pidFile string) (proc *process.Process, err error) {
 	var pid int
 	if pid, err = pkgRun.GetPidFromFile(pidFile); err != nil {
 		return
 	}
-	proc, err = getProcessFromPid(pid)
+	proc, err = GetProcessFromPid(pid)
 	return
 }
 
-func isAddressPortOpen(host string, port int) (ok bool) {
-	ok = isAddressPortOpenWithTimeout(host, port, 100*time.Millisecond)
+func IsAddressPortOpen(host string, port int) (ok bool) {
+	ok = IsAddressPortOpenWithTimeout(host, port, 100*time.Millisecond)
 	return
 }
 
-func isAddressPortOpenWithTimeout(host string, port int, timeout time.Duration) (ok bool) {
+func IsAddressPortOpenWithTimeout(host string, port int, timeout time.Duration) (ok bool) {
 	address := net.JoinHostPort(host, strconv.Itoa(port))
 	conn, err := net.DialTimeout("tcp", address, timeout)
 	ok = err == nil && conn != nil
+	return
+}
+
+func DropPrivilegesTo(userName, groupName string) (err error) {
+	if syscall.Getuid() == 0 {
+
+		var u *user.User
+		if u, err = user.Lookup(userName); err != nil {
+			return
+		}
+		var g *user.Group
+		if g, err = user.LookupGroup(groupName); err != nil {
+			return
+		}
+
+		beIo.StdoutF("switching user:group to %v:%v\n", userName, groupName)
+
+		var uid, gid int
+		if uid, err = strconv.Atoi(u.Uid); err != nil {
+			return
+		}
+		if gid, err = strconv.Atoi(g.Gid); err != nil {
+			return
+		}
+
+		if cerr, errno := C.setgid(C.__gid_t(gid)); cerr != 0 {
+			err = fmt.Errorf("set GID error: %v", errno)
+			return
+		} else if cerr, errno = C.setuid(C.__uid_t(uid)); cerr != 0 {
+			err = fmt.Errorf("set UID error: %v", errno)
+			return
+		}
+	}
 	return
 }
