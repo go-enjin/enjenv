e978ddb70d80e8034fb6340af385ee82db2ae730
Kevin C. Krinke <kevin@krinke.ca>
2023-01-24 17:07:16 -0500

    commands/niseroku: cleanup cli flags+commands; migrate to new servers (proxy+git as separate processes) from existing all-in-one-server

--- commands/niseroku/niseroku.go
+++ commands/niseroku/niseroku.go
@@ -18,21 +18,15 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
 	"os/user"
 	"strconv"
 	"syscall"
 
-	"github.com/sevlyar/go-daemon"
-	"github.com/shirou/gopsutil/v3/process"
 	"github.com/urfave/cli/v2"
 
-	"github.com/go-enjin/be/pkg/cli/run"
 	bePath "github.com/go-enjin/be/pkg/path"
 
-	"github.com/go-enjin/enjenv/pkg/basepath"
 	beIo "github.com/go-enjin/enjenv/pkg/io"
-	"github.com/go-enjin/enjenv/pkg/service/common"
 	"github.com/go-enjin/enjenv/pkg/system"
 )
 
@@ -115,28 +109,28 @@ handled directly.
 			},
 			Subcommands: []*cli.Command{
 				{
-					Name:      "start",
-					Usage:     "start the niseroku instance",
-					UsageText: app.Name + " niseroku start",
-					Action:    c.actionStart,
+					Name:      "reverse-proxy",
+					Usage:     "niseroku reverse-proxy service",
+					UsageText: app.Name + " niseroku reverse-proxy",
+					Action:    c.actionReverseProxy,
 				},
 				{
-					Name:      "stop",
-					Usage:     "stop the niseroku instance",
-					UsageText: app.Name + " niseroku stop",
-					Action:    c.actionStop,
+					Name:      "git-repository",
+					Usage:     "niseroku git-repository service",
+					UsageText: app.Name + " niseroku git-repository",
+					Action:    c.actionGitRepository,
 				},
 				{
-					Name:      "reload",
-					Usage:     "reload all niseroku configurations",
-					UsageText: app.Name + " niseroku reload",
-					Action:    c.actionReload,
+					Name:      "status",
+					Usage:     "display the status of all niseroku services",
+					UsageText: app.Name + " niseroku status",
+					Action:    c.actionStatus,
 				},
 				{
-					Name:      "restart",
-					Usage:     "restart the niseroku instance",
-					UsageText: app.Name + " niseroku restart",
-					Action:    c.actionRestart,
+					Name:      "deploy-slug",
+					Usage:     "deploy a built slug",
+					UsageText: "niseroku deploy-slug <slug.zip> [slugs.zip...]",
+					Action:    c.actionDeploySlug,
 				},
 				{
 					Name:      "fix-fs",
@@ -162,9 +156,10 @@ handled directly.
 						},
 
 						{
-							Name:   "start",
-							Usage:  "start an app slug",
-							Action: c.actionAppStart,
+							Name:      "start",
+							Usage:     "start an app slug",
+							UsageText: app.Name + " niseroku app start <name>",
+							Action:    c.actionAppStart,
 							Flags: []cli.Flag{
 								&cli.BoolFlag{
 									Name:   "slug-process",
@@ -173,9 +168,10 @@ handled directly.
 							},
 						},
 						{
-							Name:   "stop",
-							Usage:  "stop an app slug",
-							Action: c.actionAppStop,
+							Name:      "stop",
+							Usage:     "stop an app slug",
+							UsageText: app.Name + " niseroku app stop <name>",
+							Action:    c.actionAppStop,
 						},
 					},
 				},
@@ -201,7 +197,7 @@ func (c *Command) findConfig(ctx *cli.Context) (config *Config, err error) {
 	if path, err = bePath.Abs(path); err != nil {
 		return
 	}
-	config, err = InitConfig(path)
+	config, err = LoadConfig(path)
 	return
 }
 
@@ -218,201 +214,6 @@ func (c *Command) Prepare(ctx *cli.Context) (err error) {
 	return
 }
 
-func (c *Command) actionStart(ctx *cli.Context) (err error) {
-	if err = c.Prepare(ctx); err != nil {
-		return
-	}
-
-	beIo.StdoutF("# starting new service\n")
-
-	var s *Server
-	if s, err = NewServer(c.config); err != nil {
-		err = fmt.Errorf("new service error: %v", err)
-	} else if err = s.InitPidFile(); err != nil {
-		err = fmt.Errorf("init pid file error: %v", err)
-	} else if err = s.Start(); err != nil {
-		err = fmt.Errorf("error starting service: %v", err)
-	}
-	return
-}
-
-func (c *Command) actionStop(ctx *cli.Context) (err error) {
-	if err = c.Prepare(ctx); err != nil {
-		return
-	}
-	if !bePath.IsFile(c.config.Paths.PidFile) {
-		beIo.STDERR("pid file not found: %v\n", c.config.Paths.PidFile)
-		return
-	}
-	var resp string
-	if resp, err = c.CallControlCommand("shutdown"); err != nil {
-		return
-	}
-	beIo.STDOUT("%v", resp)
-	return
-}
-
-func (c *Command) actionReload(ctx *cli.Context) (err error) {
-	if err = c.Prepare(ctx); err != nil {
-		return
-	}
-
-	// if pid-file exists, read pid from file
-	// if pid is a running process: send SIGUSR1
-
-	if !bePath.IsFile(c.config.Paths.PidFile) {
-		err = fmt.Errorf("pid file not found, nothing to signal")
-		return
-	}
-
-	var proc *process.Process
-	if proc, err = common.GetProcessFromPidFile(c.config.Paths.PidFile); err == nil && proc != nil {
-		if err = proc.SendSignal(syscall.SIGUSR1); err != nil {
-			err = fmt.Errorf("error sending SIGUSR1 to process: %d - %v", proc.Pid, err)
-		} else {
-			beIo.StdoutF("sent SIGUSR1 to: %d\n", proc.Pid)
-		}
-	}
-
-	return
-}
-
-func (c *Command) actionRestart(ctx *cli.Context) (err error) {
-	if err = c.Prepare(ctx); err != nil {
-		return
-	}
-	err = fmt.Errorf("niseroku restart not implemented")
-	return
-}
-
-func (c *Command) actionAppStart(ctx *cli.Context) (err error) {
-	if err = c.Prepare(ctx); err != nil {
-		return
-	}
-	beIo.LogFile = ""
-	if ctx.NArg() != 1 {
-		err = fmt.Errorf("too many arguments")
-		return
-	}
-	appName := ctx.Args().Get(0)
-
-	var s *Server
-	var ok bool
-	var app *Application
-	var slug *Slug
-
-	if s, err = NewServer(c.config); err != nil {
-		return
-	}
-
-	if app, ok = s.LookupApp[appName]; !ok {
-		err = fmt.Errorf("app not found: %v", appName)
-		return
-	}
-
-	if slug, err = s.PrepareAppSlug(app); err != nil {
-		return
-	}
-
-	if running, ready := slug.IsRunningReady(); running && ready {
-		err = fmt.Errorf("slug is already running and ready")
-		return
-	} else if running {
-		err = fmt.Errorf("slug is already running though not ready")
-		return
-	}
-
-	if !ctx.Bool("slug-process") {
-		binPath := basepath.EnjenvBinPath
-		argv := []string{binPath, "niseroku", "app", "start", "--slug-process", appName}
-
-		dCtx := &daemon.Context{
-			Args:  argv,
-			Env:   os.Environ(),
-			Umask: 0222,
-		}
-
-		var dProc *os.Process
-		if dProc, err = dCtx.Reborn(); err != nil {
-			beIo.StderrF("error daemonizing slug process: %v - %v\n", appName, err)
-			return
-		} else if dProc != nil {
-			beIo.StdoutF("slug process started: %v\n", appName)
-			return
-		}
-		defer func() {
-			if ee := dCtx.Release(); ee != nil {
-				beIo.StderrF("error releasing daemon context: %v - %v\n", appName, err)
-			}
-		}()
-	}
-
-	if err = c.dropPrivileges(); err != nil {
-		err = fmt.Errorf("error dropping root privileges: %v", err)
-		return
-	}
-
-	var webCmd string
-	var webArgv, environ []string
-	if webCmd, webArgv, environ, err = slug.PrepareStart(app.Origin.Port); err != nil {
-		return
-	}
-
-	if ee := run.ExeWith(&run.Options{Path: slug.RunPath, Name: webCmd, Argv: webArgv, Stdout: slug.LogFile, Stderr: slug.LogFile, PidFile: slug.PidFile, Environ: environ}); ee != nil {
-		err = ee
-		return
-	}
-
-	return
-}
-
-func (c *Command) actionAppStop(ctx *cli.Context) (err error) {
-	if err = c.Prepare(ctx); err != nil {
-		return
-	}
-	beIo.LogFile = ""
-	if ctx.NArg() != 1 {
-		err = fmt.Errorf("too many arguments")
-		return
-	}
-	appName := ctx.Args().Get(0)
-
-	var s *Server
-	var ok bool
-	var app *Application
-	var slug *Slug
-
-	if err = c.dropPrivileges(); err != nil {
-		err = fmt.Errorf("error dropping root privileges: %v", err)
-		return
-	}
-
-	if s, err = NewServer(c.config); err != nil {
-		return
-	}
-	if app, ok = s.LookupApp[appName]; !ok {
-		err = fmt.Errorf("app not found: %v", appName)
-		return
-	}
-
-	if err = app.LoadAllSlugs(); err != nil {
-		err = fmt.Errorf("error loading all app slugs: %v - %v", app.Name, err)
-		return
-	}
-
-	if slug = app.GetThisSlug(); slug != nil {
-		if slug.Stop() {
-			beIo.STDOUT("slug process stopped: %v\n", app.Name)
-		} else {
-			beIo.STDOUT("slug process already stopped: %v\n", app.Name)
-		}
-	} else {
-		err = fmt.Errorf("error getting this slug for app: %v", app.Name)
-	}
-
-	return
-}
-
 func (c *Command) dropPrivileges() (err error) {
 	if syscall.Getuid() == 0 {
 
@@ -425,7 +226,7 @@ func (c *Command) dropPrivileges() (err error) {
 			return
 		}
 
-		beIo.StdoutF("switching user:group to %v:%v\n", c.config.RunAs.User, c.config.RunAs.Group)
+		// beIo.StdoutF("switching user:group to %v:%v\n", c.config.RunAs.User, c.config.RunAs.Group)
 
 		var uid, gid int
 		if uid, err = strconv.Atoi(u.Uid); err != nil {
