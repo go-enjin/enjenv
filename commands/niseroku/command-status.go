771d01ed9099b66ebd7b25cf2f5db0585d660669
Kevin C. Krinke <kevin@krinke.ca>
2023-01-27 15:55:20 -0500

    commands/niseroku (status): add nice, cpu and mem statistics to status output

--- commands/niseroku/command-status.go
+++ commands/niseroku/command-status.go
@@ -21,10 +21,12 @@ import (
 	"strconv"
 	"text/tabwriter"
 
+	"github.com/shirou/gopsutil/v3/process"
 	"github.com/urfave/cli/v2"
 
 	"github.com/go-enjin/be/pkg/maps"
 	bePath "github.com/go-enjin/be/pkg/path"
+
 	beIo "github.com/go-enjin/enjenv/pkg/io"
 	"github.com/go-enjin/enjenv/pkg/service/common"
 )
@@ -36,9 +38,37 @@ func (c *Command) actionStatus(ctx *cli.Context) (err error) {
 
 	buf := bytes.NewBuffer([]byte(""))
 	tw := tabwriter.NewWriter(io.Writer(buf), 3, 2, 2, ' ', tabwriter.FilterHTML)
-	_, _ = tw.Write([]byte("[ SERVICE ]\t[ PID ]\t[ PORT ]\n"))
-	// _, _ = tw.Write([]byte(" \t \t \n"))
-	report := func(name string, running, ready bool, pid, port int) {
+	_, _ = tw.Write([]byte("[ SERVICE ]\t[ PID ]\t[ PORT ]\t[ NICE ]\t[ CPU ]\t[ MEM ]\n"))
+	report := func(name string, running, ready bool, pid, port int, proc *process.Process) {
+		cpu, mem, nice := "-", "-", "-"
+		if proc != nil {
+			cpuPercent, _ := proc.CPUPercent()
+			memPercent, _ := proc.MemoryPercent()
+			if children, ee := proc.Children(); ee == nil {
+				var childConns int
+				var childCpuTotal float64
+				var childMemTotal float32
+				for _, child := range children {
+					if cir, _ := child.IsRunning(); cir {
+						if cp, eee := child.CPUPercent(); eee == nil {
+							childCpuTotal += cp
+						}
+						if mp, eee := child.MemoryPercent(); eee == nil {
+							childMemTotal += mp
+						}
+						if connections, eee := proc.Connections(); eee == nil {
+							childConns += len(connections)
+						}
+					}
+				}
+				cpuPercent += childCpuTotal
+				memPercent += childMemTotal
+			}
+			cpu = fmt.Sprintf("%.01f", cpuPercent)
+			mem = fmt.Sprintf("%.01f", memPercent)
+			niceVal, _ := proc.Nice()
+			nice = fmt.Sprintf("%d", 20-niceVal)
+		}
 		var runningMsg, readyMsg string
 		if running {
 			runningMsg = strconv.Itoa(pid)
@@ -50,19 +80,20 @@ func (c *Command) actionStatus(ctx *cli.Context) (err error) {
 		} else {
 			readyMsg = "-"
 		}
-		// beIo.STDOUT("%v\t\t%v\n", name, message)
-		_, _ = tw.Write([]byte(fmt.Sprintf("%v\t%v\t%v\n", name, runningMsg, readyMsg)))
+		_, _ = tw.Write([]byte(fmt.Sprintf("%v\t%v\t%v\t%v\t%v\t%v\n", name, runningMsg, readyMsg, nice, cpu, mem)))
 	}
 
 	proxyPid := -1
 	proxyPort := -1
 	proxyIsRunning := false
 	proxyIsReady := false
+	var proxyProc *process.Process
 	if bePath.IsFile(c.config.Paths.ProxyPidFile) {
 		if proc, ee := common.GetProcessFromPidFile(c.config.Paths.ProxyPidFile); ee == nil {
 			if running, ee := proc.IsRunning(); ee == nil {
 				if proxyIsRunning = running; running {
 					proxyPid = int(proc.Pid)
+					proxyProc = proc
 				}
 			}
 		}
@@ -71,17 +102,19 @@ func (c *Command) actionStatus(ctx *cli.Context) (err error) {
 			proxyPort = c.config.Ports.Http
 		}
 	}
-	report("reverse-proxy", proxyIsRunning, proxyIsReady, proxyPid, proxyPort)
+	report("reverse-proxy", proxyIsRunning, proxyIsReady, proxyPid, proxyPort, proxyProc)
 
 	repoPid := -1
 	repoPort := -1
 	repoIsRunning := false
 	repoIsReady := false
+	var repoProc *process.Process
 	if bePath.IsFile(c.config.Paths.RepoPidFile) {
 		if proc, ee := common.GetProcessFromPidFile(c.config.Paths.RepoPidFile); ee == nil {
 			if running, eee := proc.IsRunning(); eee == nil {
 				if repoIsRunning = running; running {
 					repoPid = int(proc.Pid)
+					repoProc = proc
 				}
 			}
 		}
@@ -90,18 +123,20 @@ func (c *Command) actionStatus(ctx *cli.Context) (err error) {
 			repoPort = c.config.Ports.Git
 		}
 	}
-	report("git-repository", repoIsRunning, repoIsReady, repoPid, repoPort)
+	report("git-repository", repoIsRunning, repoIsReady, repoPid, repoPort, repoProc)
 
 	_, _ = tw.Write([]byte(" \t \t \n"))
-	_, _ = tw.Write([]byte("[ APPLICATION ]\t[ PID ]\t[ PORT ]\n"))
+	_, _ = tw.Write([]byte("[ APPLICATION ]\t[ PID ]\t[ PORT ]\t[ NICE ]\t[ CPU ]\t[ MEM ]\n"))
 	for _, app := range maps.ValuesSortedByKeys(c.config.Applications) {
 		var pid, port int
 		var running, ready bool
+		var appProc *process.Process
 		if thisSlug := app.GetThisSlug(); thisSlug != nil {
 			if proc, ee := thisSlug.GetBinProcess(); ee == nil {
 				if r, eee := proc.IsRunning(); eee == nil {
 					if running = r; r {
 						pid = int(proc.Pid)
+						appProc = proc
 					}
 				}
 			}
@@ -111,7 +146,7 @@ func (c *Command) actionStatus(ctx *cli.Context) (err error) {
 				}
 			}
 		}
-		report(app.Name, running, ready, pid, port)
+		report(app.Name, running, ready, pid, port, appProc)
 	}
 
 	_ = tw.Flush()
