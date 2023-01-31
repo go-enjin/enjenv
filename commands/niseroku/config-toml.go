46ff92d89f9158ee040b53803ac2f89482b31609
Kevin C. Krinke <kevin@krinke.ca>
2023-01-31 13:36:11 -0500

    commands/niseroku (config comments): updated all comments to include reload/restart notes; added trailing comment-gap-line to comment line blocks (readability is better); typo-fix for ready-interval comment statement

--- commands/niseroku/config-toml.go
+++ commands/niseroku/config-toml.go
@@ -16,10 +16,24 @@ package niseroku
 
 var configTomlComments = TomlComments{
 	{
+		Statement: "__header__",
+		Lines: []string{
+			": BEGIN NISEROKU CONFIG - enjenv niseroku config --help",
+		},
+	},
+	{
+		Statement: "__footer__",
+		Lines: []string{
+			": END NISEROKU CONFIG - enjenv niseroku config --help",
+		},
+	},
+	{
 		Statement: "bind-addr",
 		Lines: []string{
 			": bind-addr         (string)",
 			":     * ip address for niseroku services to bind listeners",
+			":     * requires niseroku-{proxy,repos} restart if changed",
+			"",
 		},
 	},
 	{
@@ -28,6 +42,8 @@ var configTomlComments = TomlComments{
 			": enable-ssl        (bool)",
 			":     * reverse-proxy will bind an HTTPS autocert listener",
 			":     * requires account-email to be set",
+			":     * requires niseroku-proxy restart if changed",
+			"",
 		},
 	},
 	{
@@ -36,6 +52,8 @@ var configTomlComments = TomlComments{
 			": account-email     (email@address.string)",
 			":     * email address used for the Let's Encrypt account",
 			":     * implies agreement with Let's Encrypt terms of service",
+			":     * requires niseroku-proxy restart if changed",
+			"",
 		},
 	},
 	{
@@ -43,6 +61,8 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": buildpack-path    (path)",
 			":     * specifies an enjenv-heroku-buildpack checkout to use",
+			":     * requires niseroku-repos reload or restart if changed",
+			"",
 		},
 	},
 	{
@@ -51,6 +71,8 @@ var configTomlComments = TomlComments{
 			": log-file          (path)",
 			":     * specifies the path to use for logging services",
 			":     * both reverse-proxy and git-repository log to this file",
+			":     * requires niseroku-{proxy,repos} reload or restart if changed",
+			"",
 		},
 	},
 	{
@@ -59,6 +81,8 @@ var configTomlComments = TomlComments{
 			": slug-nice         (number: -20 to 20)",
 			":     * renice all slugs run to the given priority",
 			":     * be careful with this setting!",
+			":     * requires niseroku app restart if changed",
+			"",
 		},
 	},
 	{
@@ -66,6 +90,8 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": [include-slugs]   (section)",
 			":     * configures when to include slugs in the niseroku lifecycle",
+			":     * requires niseroku-proxy reload or restart if changed",
+			"",
 		},
 	},
 	{
@@ -82,6 +108,8 @@ var configTomlComments = TomlComments{
 			": [timeouts]        (section)",
 			":     * global reverse-proxy timeout settings",
 			":     * uses the Go time.Duration format, see: https://pkg.go.dev/time#ParseDuration",
+			":     * requires niseroku-proxy reload or restart if changed",
+			"",
 		},
 	},
 	{
@@ -89,13 +117,15 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": slug-startup      (time.Duration)",
 			":     * maximum time to allow slugs to open the expected port",
+			"",
 		},
 	},
 	{
-		Statement: "read-interval",
+		Statement: "ready-interval",
 		Lines: []string{
 			": ready-interval    (time.Duration)",
 			":     * frequency at which niseroku checks expected ports to open",
+			"",
 		},
 	},
 	{
@@ -103,6 +133,7 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": origin-request    (time.Duration)",
 			":     * maximum time to allow slugs to perform a given request",
+			"",
 		},
 	},
 	{
@@ -110,54 +141,64 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": [proxy-limit]     (section)",
 			":     * reverse-proxy request rate-limiting settings",
+			":     * requires niseroku-proxy reload or restart if changed",
+			"",
 		},
 	},
 	{
 		Statement: "ttl",
 		Lines: []string{
 			": ttl (time.Duration) - rate-limiter cached values lifetime",
+			"",
 		},
 	},
 	{
 		Statement: "max",
 		Lines: []string{
 			": max (int) - concurrent requests allowed before rate limiting",
+			"",
 		},
 	},
 	{
 		Statement: "burst",
 		Lines: []string{
 			": burst (int) - concurrent requests allowed within a brief timeframe before rate limiting",
+			"",
 		},
 	},
 	{
 		Statement: "max-delay",
 		Lines: []string{
 			": max-delay (time.Duration) - maximum time to delay requests before 429 response",
+			"",
 		},
 	},
 	{
 		Statement: "delay-scale",
 		Lines: []string{
 			": delay-scale (int) - number of limit-check intervals within the max-delay timeframe",
+			"",
 		},
 	},
 	{
 		Statement: "log-allowed",
 		Lines: []string{
 			": log-allowed (bool) - log when already delayed requests are allowed to pass",
+			"",
 		},
 	},
 	{
 		Statement: "log-delayed",
 		Lines: []string{
 			": log-delayed (bool) - log each time a request is delayed by rate-limiting",
+			"",
 		},
 	},
 	{
 		Statement: "log-limited",
 		Lines: []string{
 			": log-limited (bool) - log each time a request is limited (429 response)",
+			"",
 		},
 	},
 	{
@@ -165,6 +206,9 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": [run-as]          (section)",
 			":     * when run as root, drop privileges to the specified user and group",
+			":     * requires niseroku-{proxy,repos} restart if changed",
+			":     * may also require \"sudo enjenv niseroku fix-fs\" to correct file ownership errors",
+			"",
 		},
 	},
 	{
@@ -172,18 +216,23 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": [ports]           (section)",
 			":     * all ports specified are used with the bind-addr setting",
+			"",
 		},
 	},
 	{
 		Statement: "git",
 		Lines: []string{
 			": git               (number: 1 to 65534)",
+			":     * requires niseroku-repos restart if changed",
+			"",
 		},
 	},
 	{
 		Statement: "http",
 		Lines: []string{
 			": http               (number: 1 to 65534)",
+			":     * requires niseroku-proxy restart if changed",
+			"",
 		},
 	},
 	{
@@ -191,6 +240,8 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": https              (number: 1 to 65534)",
 			":     * setting to anything other than 443 has not been tested",
+			":     * requires niseroku-proxy restart if changed",
+			"",
 		},
 	},
 	{
@@ -198,6 +249,7 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": app-start          (number: 1 to 65534)",
 			":     * start of application port range",
+			"",
 		},
 	},
 	{
@@ -205,6 +257,7 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": app-end            (number: 1 to 65534)",
 			":     * end of application port range",
+			"",
 		},
 	},
 	{
@@ -212,6 +265,9 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": [paths]            (section)",
 			":     * top-levels of where niseroku files live",
+			":     * requires niseroku-{proxy,repos} restart if changed",
+			":     * may also require \"sudo enjenv niseroku fix-fs\" to correct file ownership errors",
+			"",
 		},
 	},
 	{
@@ -219,6 +275,7 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": etc                (path)",
 			":     * where configuration files live",
+			"",
 		},
 	},
 	{
@@ -226,6 +283,7 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": tmp                (path)",
 			":     * where temporary files live",
+			"",
 		},
 	},
 	{
@@ -233,6 +291,7 @@ var configTomlComments = TomlComments{
 		Lines: []string{
 			": var                (path)",
 			":     * where persistent files live",
+			"",
 		},
 	},
 }
