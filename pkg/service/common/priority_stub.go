d7bf6b73721d42c04f23121910070090948f6bb7
Kevin C. Krinke <kevin@krinke.ca>
2023-02-05 12:47:59 -0500

    pkg/service/common: unix.SetPriority wrappers

---
+++ pkg/service/common/priority_stub.go
@@ -0,0 +1,27 @@
+//go:build !unix
+
+// Copyright (c) 2023  The Go-Enjin Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+func SetPidPriority(pid, priority int) (err error) {
+	// nop, unsupported platform
+	return
+}
+
+func SetPgrpPriority(pid, priority int) (err error) {
+	// nop, unsupported platform
+	return
+}
