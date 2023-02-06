1c42d9187eab2dff3c8ea508bdb0dc3b0a66f041
Kevin C. Krinke <kevin@krinke.ca>
2023-02-06 16:05:59 -0500

    commands/niseroku (status): refactored status command to use new Watching construct (and underlying cpuinfo)

--- commands/niseroku/command-status.go
+++ commands/niseroku/command-status.go
@@ -24,14 +24,11 @@ import (
 	"text/tabwriter"
 	"time"
 
-	"github.com/shirou/gopsutil/v3/process"
-	"github.com/urfave/cli/v2"
-
 	"github.com/go-enjin/be/pkg/maps"
 	bePath "github.com/go-enjin/be/pkg/path"
+	"github.com/urfave/cli/v2"
 
 	beIo "github.com/go-enjin/enjenv/pkg/io"
-	"github.com/go-enjin/enjenv/pkg/service/common"
 )
 
 func (c *Command) actionStatus(ctx *cli.Context) (err error) {
@@ -39,196 +36,131 @@ func (c *Command) actionStatus(ctx *cli.Context) (err error) {
 		return
 	}
 
+	watching := NewWatching(c.config, 500*time.Millisecond)
+	if err = watching.Start(); err != nil {
+		return
+	}
+	time.Sleep(600 * time.Millisecond)
+	snapshot := watching.Snapshot()
+	watching.Stop()
+	c.statusDisplayWatchingSnapshot(&snapshot)
+	c.statusDisplayWatchingProxyLimits()
+	return
+}
+
+func (c *Command) statusDisplayWatchingSnapshot(snapshot *WatchSnapshot) {
 	buf := bytes.NewBuffer([]byte(""))
-	tw := tabwriter.NewWriter(io.Writer(buf), 3, 2, 2, ' ', tabwriter.FilterHTML)
-	_, _ = tw.Write([]byte("[ SERVICE ]\t[ PID ]\t[ PORT ]\t[ NICE ]\t[ CPU ]\t[ MEM ]\t[ THREADS ]\n"))
-	report := func(name string, running, ready bool, pid, port int, proc *process.Process) {
-		cpu, mem, nice, threads := "-", "-", "-", "-"
-		if proc != nil {
-			cpuPercent, _ := proc.CPUPercent()
-			memPercent, _ := proc.MemoryPercent()
-			numThreads, _ := proc.NumThreads()
-			var numChildren, numChildThreads int
-			if children, ee := proc.Children(); ee == nil {
-				var childCpuTotal float64
-				var childMemTotal float32
-				for _, child := range children {
-					if cir, ce := child.IsRunning(); ce == nil && cir {
-						numChildren += 1
-						childThreads, _ := child.NumThreads()
-						numChildThreads += int(childThreads)
-						if cp, eee := child.CPUPercent(); eee == nil {
-							if childCpuTotal < cp {
-								childCpuTotal = cp
-							}
-						}
-						if mp, eee := child.MemoryPercent(); eee == nil {
-							if childMemTotal < mp {
-								childMemTotal = mp
-							}
-						}
-					}
-				}
-				cpuPercent += childCpuTotal
-				memPercent += childMemTotal
-			}
-			cpu = fmt.Sprintf("%.01f", cpuPercent)
-			mem = fmt.Sprintf("%.01f", memPercent)
-			niceVal, _ := proc.Nice()
-			nice = fmt.Sprintf("%d", 20-niceVal)
-			if numChildren > 0 {
-				threads = fmt.Sprintf("%v+(%d/%d)", numThreads, numChildren, numChildThreads)
-			} else {
-				threads = fmt.Sprintf("%v", numThreads)
-			}
-		}
-		var runningMsg, readyMsg string
-		if running {
-			runningMsg = strconv.Itoa(pid)
+	tw := tabwriter.NewWriter(io.Writer(buf), 8, 2, 2, ' ', tabwriter.FilterHTML)
+
+	writeEntry := func(stat WatchProc) {
+		var pid, ports, nice, cpu, mem, num string
+		if stat.Pid <= 0 {
+			pid, nice, cpu, mem, num = "-", "-", "-", "-", "-"
 		} else {
-			runningMsg = "-"
+			pid = strconv.Itoa(stat.Pid)
+			nice = fmt.Sprintf("%d", stat.Nice)
+			cpu = fmt.Sprintf("%.2f", stat.Cpu)
+			mem = fmt.Sprintf("%.2f", stat.Mem)
+			num = fmt.Sprintf("%d (%d)", stat.Num, stat.Threads)
 		}
-		if ready {
-			readyMsg = strconv.Itoa(port)
-		} else {
-			readyMsg = "-"
+		for idx, port := range stat.Ports {
+			if idx > 0 {
+				ports += ","
+			}
+			ports += strconv.Itoa(port)
 		}
-		_, _ = tw.Write([]byte(fmt.Sprintf("%v\t%v\t%v\t%v\t%v\t%v\t%v\n", name, runningMsg, readyMsg, nice, cpu, mem, threads)))
+		_, _ = tw.Write([]byte(fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", stat.Name, pid, ports, nice, cpu, mem, num)))
 	}
 
-	proxyPid := -1
-	proxyPort := -1
-	proxyIsRunning := false
-	proxyIsReady := false
-	var proxyProc *process.Process
-	if bePath.IsFile(c.config.Paths.ProxyPidFile) {
-		if proc, ee := common.GetProcessFromPidFile(c.config.Paths.ProxyPidFile); ee == nil {
-			if running, ee := proc.IsRunning(); ee == nil {
-				if proxyIsRunning = running; running {
-					proxyPid = int(proc.Pid)
-					proxyProc = proc
-				}
-			}
-		}
-		if common.IsAddressPortOpen(c.config.BindAddr, c.config.Ports.Http) {
-			proxyIsReady = true
-			proxyPort = c.config.Ports.Http
-		}
-	}
-	report("reverse-proxy", proxyIsRunning, proxyIsReady, proxyPid, proxyPort, proxyProc)
+	// SERVICES
 
-	repoPid := -1
-	repoPort := -1
-	repoIsRunning := false
-	repoIsReady := false
-	var repoProc *process.Process
-	if bePath.IsFile(c.config.Paths.RepoPidFile) {
-		if proc, ee := common.GetProcessFromPidFile(c.config.Paths.RepoPidFile); ee == nil {
-			if running, eee := proc.IsRunning(); eee == nil {
-				if repoIsRunning = running; running {
-					repoPid = int(proc.Pid)
-					repoProc = proc
-				}
-			}
-		}
-		if common.IsAddressPortOpen(c.config.BindAddr, c.config.Ports.Git) {
-			repoIsReady = true
-			repoPort = c.config.Ports.Git
-		}
-	}
-	report("git-repository", repoIsRunning, repoIsReady, repoPid, repoPort, repoProc)
-
-	_, _ = tw.Write([]byte(" \t \t \n"))
-	_, _ = tw.Write([]byte("[ APPLICATION ]\t[ PID ]\t[ PORT ]\t[ NICE ]\t[ CPU ]\t[ MEM ]\t[ THREADS ]\n"))
-	for _, app := range maps.ValuesSortedByKeys(c.config.Applications) {
-		var pid, port int
-		var running, ready bool
-		var appProc *process.Process
-		if thisSlug := app.GetThisSlug(); thisSlug != nil {
-			if proc, ee := thisSlug.GetBinProcess(); ee == nil {
-				if r, eee := proc.IsRunning(); eee == nil {
-					if running = r; r {
-						pid = int(proc.Pid)
-						appProc = proc
-					}
-				}
-			}
-			if ready = thisSlug.IsReady(); ready {
-				if port = thisSlug.Port; port <= 0 {
-					port = thisSlug.App.Origin.Port
-				}
-			}
-		}
-		report(app.Name, running, ready, pid, port, appProc)
+	_, _ = tw.Write([]byte("[ SERVICE ]\t[ PID ]\t[ PORTS ]\t[ PRIORITY ]\t[ CPU ]\t[ MEM ]\t[ PROCS ]\n"))
+	for _, stat := range snapshot.Services {
+		writeEntry(stat)
 	}
 
+	// APPLICATIONS
+	_, _ = tw.Write([]byte("\t\t\t\t\t\n"))
+	_, _ = tw.Write([]byte("[ APPLICATION ]\t[ PID ]\t[ PORTS ]\t[ PRIORITY ]\t[ CPU ]\t[ MEM ]\t[ PROCS ]\n"))
+	for _, stat := range snapshot.Applications {
+		writeEntry(stat)
+	}
+
+	// Output
 	_ = tw.Flush()
 	beIo.STDOUT(buf.String())
+	return
+}
+
+func (c *Command) statusDisplayWatchingProxyLimits() {
+	buf := bytes.NewBuffer([]byte(""))
+	tw := tabwriter.NewWriter(io.Writer(buf), 8, 2, 2, ' ', tabwriter.FilterHTML)
 
 	if c.config.SignalDumpStatsReverseProxy() {
-		time.Sleep(100 * time.Millisecond)
-		if bePath.IsFile(c.config.Paths.ProxyDumpStats) {
-			data, _ := os.ReadFile(c.config.Paths.ProxyDumpStats)
-			var rTotal int64
-			rHosts := make(map[string]int64)
-			rAddrs := make(map[string]int64)
-			var dTotal int64
-			dHosts := make(map[string]int64)
-			dAddrs := make(map[string]int64)
-			for _, line := range strings.Split(string(data), "\n") {
-				line = strings.TrimSpace(line)
-				if parts := strings.Split(line, "="); len(parts) == 2 {
-					nameParts := strings.Split(parts[0], ",")
-					switch len(nameParts) {
-					case 2:
-						if nameParts[0] == "host" {
-							rHosts[nameParts[1]], _ = strconv.ParseInt(parts[1], 10, 64)
-						} else if nameParts[0] == "addr" {
-							rAddrs[nameParts[1]], _ = strconv.ParseInt(parts[1], 10, 64)
+		for i := 0; bePath.IsFile(c.config.Paths.ProxyDumpStats) == false; i++ {
+			if i == 10 {
+				// dump stats not found within 1s
+				return
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+
+		data, _ := os.ReadFile(c.config.Paths.ProxyDumpStats)
+		var rTotal int64
+		rHosts := make(map[string]int64)
+		rAddrs := make(map[string]int64)
+		var dTotal int64
+		dHosts := make(map[string]int64)
+		dAddrs := make(map[string]int64)
+		for _, line := range strings.Split(string(data), "\n") {
+			line = strings.TrimSpace(line)
+			if parts := strings.Split(line, "="); len(parts) == 2 {
+				nameParts := strings.Split(parts[0], ",")
+				switch len(nameParts) {
+				case 2:
+					if nameParts[0] == "host" {
+						rHosts[nameParts[1]], _ = strconv.ParseInt(parts[1], 10, 64)
+					} else if nameParts[0] == "addr" {
+						rAddrs[nameParts[1]], _ = strconv.ParseInt(parts[1], 10, 64)
+					}
+				case 3:
+					if nameParts[0] == "delay" {
+						if nameParts[1] == "host" {
+							dHosts[nameParts[2]], _ = strconv.ParseInt(parts[1], 10, 64)
+						} else if nameParts[1] == "addr" {
+							dAddrs[nameParts[2]], _ = strconv.ParseInt(parts[1], 10, 64)
 						}
-					case 3:
-						if nameParts[0] == "delay" {
-							if nameParts[1] == "host" {
-								dHosts[nameParts[2]], _ = strconv.ParseInt(parts[1], 10, 64)
-							} else if nameParts[1] == "addr" {
-								dAddrs[nameParts[2]], _ = strconv.ParseInt(parts[1], 10, 64)
-							}
-						}
-					default:
-						if parts[0] == "__total__" {
-							rTotal, _ = strconv.ParseInt(parts[1], 10, 64)
-						} else if parts[0] == "__delay__" {
-							dTotal, _ = strconv.ParseInt(parts[1], 10, 64)
-						}
+					}
+				default:
+					if parts[0] == "__total__" {
+						rTotal, _ = strconv.ParseInt(parts[1], 10, 64)
+					} else if parts[0] == "__delay__" {
+						dTotal, _ = strconv.ParseInt(parts[1], 10, 64)
 					}
 				}
 			}
-
-			beIo.STDOUT("\n")
-			buf = bytes.NewBuffer([]byte(""))
-			tw = tabwriter.NewWriter(io.Writer(buf), 8, 2, 2, ' ', tabwriter.FilterHTML)
-			_, _ = tw.Write([]byte("[ PROXY LIMITS ]\t[ CURRENT ]\t[ DELAYED ]\n"))
-			_, _ = tw.Write([]byte(fmt.Sprintf("(total)\t%d\t%d\n", rTotal, dTotal)))
-			if len(rHosts) > 0 {
-				_, _ = tw.Write([]byte("\t\t\n"))
-				_, _ = tw.Write([]byte("[ HOST LIMITS ]\t[ CURRENT ]\t[ DELAYED ]\n"))
-				for _, key := range maps.SortedKeys(rHosts) {
-					dHostValue, _ := dHosts[key]
-					_, _ = tw.Write([]byte(fmt.Sprintf("%s\t%d\t%d\n", key, rHosts[key], dHostValue)))
-				}
-			}
-			if len(rAddrs) > 0 {
-				_, _ = tw.Write([]byte("\t\t\n"))
-				_, _ = tw.Write([]byte("[ ADDR LIMITS ]\t[ CURRENT ]\t[ DELAYED ]\n"))
-				for _, key := range maps.SortedKeys(rAddrs) {
-					dAddrValue, _ := dAddrs[key]
-					_, _ = tw.Write([]byte(fmt.Sprintf("%s\t%d\t%d\n", key, rAddrs[key], dAddrValue)))
-				}
-			}
-			_ = tw.Flush()
-			beIo.STDOUT(buf.String())
 		}
-	}
 
-	return
+		beIo.STDOUT("\n")
+		_, _ = tw.Write([]byte("[ PROXY LIMITS ]\t[ CURRENT ]\t[ DELAYED ]\n"))
+		_, _ = tw.Write([]byte(fmt.Sprintf("(total)\t%d\t%d\n", rTotal, dTotal)))
+		if len(rHosts) > 0 {
+			_, _ = tw.Write([]byte("\t\t\n"))
+			_, _ = tw.Write([]byte("[ HOST LIMITS ]\t[ CURRENT ]\t[ DELAYED ]\n"))
+			for _, key := range maps.SortedKeys(rHosts) {
+				dHostValue, _ := dHosts[key]
+				_, _ = tw.Write([]byte(fmt.Sprintf("%s\t%d\t%d\n", key, rHosts[key], dHostValue)))
+			}
+		}
+		if len(rAddrs) > 0 {
+			_, _ = tw.Write([]byte("\t\t\n"))
+			_, _ = tw.Write([]byte("[ ADDR LIMITS ]\t[ CURRENT ]\t[ DELAYED ]\n"))
+			for _, key := range maps.SortedKeys(rAddrs) {
+				dAddrValue, _ := dAddrs[key]
+				_, _ = tw.Write([]byte(fmt.Sprintf("%s\t%d\t%d\n", key, rAddrs[key], dAddrValue)))
+			}
+		}
+		_ = tw.Flush()
+		beIo.STDOUT(buf.String())
+	}
 }
