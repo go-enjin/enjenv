5638ad005ba1a7b648d68edd0f6364156daa3f6a
Kevin C. Krinke <kevin@krinke.ca>
2023-02-07 13:44:37 -0500

    _templates: multitail wrapper script for tailing niseroku logs

---
+++ _templates/niseroku-tail.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+APP_ARGS=
+
+for TOML in $(ls -1 /etc/niseroku/apps.d/*.toml)
+do
+    NAME=$(basename "${TOML}" ".toml")
+    INFO_LOG="/var/lib/niseroku/logs.d/${NAME}.info.log"
+    ERROR_LOG="/var/lib/niseroku/logs.d/${NAME}.error.log"
+    ACCESS_LOG="/var/lib/niseroku/logs.d/${NAME}.access.log"
+    APP_ARGS="${APP_ARGS} -t ${NAME} --label [FAIL] -iw ${ERROR_LOG} 1s"
+    APP_ARGS="${APP_ARGS} -t ${NAME} --label [INFO] -Iw ${INFO_LOG} 1s"
+    APP_ARGS="${APP_ARGS} -t ${NAME} --label [HTTP] -Iw ${ACCESS_LOG} 1s"
+done
+
+exec multitail \
+     -x 'niseroku-tail (%h)' \
+     -H -T -du -N 1 \
+     --basename \
+     --no-repeat \
+     --retry-all \
+     --follow-all \
+     --mark-interval 300 \
+     /var/log/niseroku.log \
+     ${APP_ARGS}
