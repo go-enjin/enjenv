3456ad42384df2dc34a78cd0aff3307cf5ab49a2
Kevin C. Krinke <kevin@krinke.ca>
2023-02-17 21:07:37 -0500

    commands/niseroku (slug): refactoring for new slug instances; code cosmetic changes

--- commands/niseroku/slug.go
+++ commands/niseroku/slug.go
@@ -17,30 +17,24 @@ package niseroku
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"strconv"
+	"path/filepath"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/shirou/gopsutil/v3/process"
 
-	"github.com/go-enjin/be/pkg/cli/run"
+	"github.com/go-enjin/be/pkg/maps"
 	bePath "github.com/go-enjin/be/pkg/path"
-
-	"github.com/go-enjin/enjenv/pkg/service/common"
 )
 
 type Slug struct {
 	App     *Application
 	Name    string
+	Commit  string
 	Archive string
-	RunPath string
-	PidFile string
-	LogFile string
 
-	Port int `toml:"-"`
+	Instances []*SlugInstance
 
 	sync.RWMutex
 }
@@ -50,292 +44,46 @@ func NewSlugFromZip(app *Application, archive string) (slug *Slug, err error) {
 		err = fmt.Errorf("slug not found: %v", archive)
 		return
 	}
-	slugName := bePath.Base(archive)
-	runPath := app.Config.Paths.TmpRun + "/" + slugName
-	pidFile := runPath + ".pid"
-	logFile := app.Config.Paths.VarLogs + "/" + app.Name + ".log"
-
 	slug = &Slug{
 		App:     app,
-		Name:    slugName,
+		Name:    bePath.Base(archive),
 		Archive: archive,
-		RunPath: runPath,
-		PidFile: pidFile,
-		LogFile: logFile,
 	}
+	if RxSlugArchiveName.MatchString(slug.Archive) {
+		m := RxSlugArchiveName.FindAllStringSubmatch(slug.Archive, 1)
+		slug.Commit = m[0][2]
+	}
+	slug.ReloadSlugInstances()
 	return
 }
 
-func (s *Slug) String() (text string) {
-	running, ready := s.IsRunningReady()
-	text = fmt.Sprintf("{slug=%v,port=%v;running=%v;ready=%v;}", s.Name, s.Port, running, ready)
-	return
-}
+func (s *Slug) ReloadSlugInstances() {
+	s.Lock()
+	defer s.Unlock()
 
-func (s *Slug) Compare(other *Slug) (sameSlug, samePort bool) {
-	sameSlug = s.Name == other.Name &&
-		s.App.Name == other.App.Name &&
-		s.Archive == other.Archive &&
-		s.PidFile == other.PidFile &&
-		s.RunPath == other.RunPath
-	samePort = s.Port == other.Port
-	return
-}
+	s.Instances = make([]*SlugInstance, 0)
 
-func (s *Slug) Unpack() (err error) {
-	if bePath.IsDir(s.RunPath) {
-		s.App.LogInfoF("slug already unpacked: %v\n", s.Name)
-		return
-	}
-	if err = bePath.Mkdir(s.RunPath); err != nil {
-		s.App.LogErrorF("error making run path: %v - %v\n", s.RunPath, err)
-		return
-	}
-	s.App.LogInfoF("unzipping: %v - %v\n", s.RunPath, s.Archive)
-	var unzipBin string
-	if unzipBin, err = exec.LookPath("unzip"); err != nil {
-		s.App.LogErrorF("error finding unzip program: %v\n", err)
-		return
-	}
-	if err = run.ExeWith(&run.Options{Path: s.RunPath, Name: unzipBin, Argv: []string{"-qq", s.Archive}}); err != nil {
-		s.App.LogErrorF("error executing unzip: %v - %v\n", unzipBin, err)
-		return
-	}
-	return
-}
-
-func (s *Slug) ReadProcfile() (web string, err error) {
-	// TODO: ReadProcfile needs to return a map[string]string containing all Procfile entries
-	if bePath.IsDir(s.RunPath) {
-		procfile := s.RunPath + "/Procfile"
-		if bePath.IsFile(procfile) {
-			var data []byte
-			if data, err = os.ReadFile(procfile); err != nil {
-				return
+	hashes := make(map[string]bool)
+	if paths, err := bePath.List(s.App.Config.Paths.TmpRun); err == nil {
+		for _, path := range paths {
+			baseName := filepath.Base(path)
+			if strings.HasPrefix(baseName, s.Name) {
+				if RxSlugRunningName.MatchString(path) {
+					m := RxSlugRunningName.FindAllStringSubmatch(path, 1)
+					// appName, commitId, hash, extn := m[0][1], m[0][2], m[0][3], m[0][4]
+					hash := m[0][3]
+					hashes[hash] = true
+				}
 			}
-			procdata := string(data)
-			if RxSlugProcfileWebEntry.MatchString(procdata) {
-				m := RxSlugProcfileWebEntry.FindAllStringSubmatch(procdata, 1)
-				web = m[0][1]
-			} else {
-				err = fmt.Errorf("slug procfile missing web entry:\n%v", procdata)
-			}
-		} else {
-			err = fmt.Errorf("slug missing Procfile: %v", s.Name)
-		}
-	} else {
-		err = fmt.Errorf("slug is not unpacked yet")
-	}
-	return
-}
-
-func (s *Slug) IsReady() (ready bool) {
-	port := s.Port
-	if port <= 0 {
-		port = s.App.Origin.Port
-	}
-	ready = common.IsAddressPortOpen(s.App.Origin.Host, port)
-	return
-}
-
-func (s *Slug) IsRunning() (running bool) {
-	s.RLock()
-	defer s.RUnlock()
-	if proc, err := s.GetBinProcess(); err == nil && proc != nil {
-		running = proc.Pid > 0
-	}
-	return
-}
-
-func (s *Slug) IsRunningReady() (running, ready bool) {
-	running = s.IsRunning()
-	ready = s.IsReady()
-	return
-}
-
-func (s *Slug) GetBinProcess() (proc *process.Process, err error) {
-	proc, err = common.GetProcessFromPidFile(s.PidFile)
-	// if proc, err = GetProcessFromPidFile(s.PidFile); err == nil && proc != nil {
-	// 	if s.App.BinName != "" {
-	// 		var ee error
-	// 		var procName string
-	// 		if procName, ee = proc.Name(); ee == nil {
-	// 			if procName = filepath.Base(procName); procName == s.App.BinName {
-	// 				// top process is the app binary
-	// 				return
-	// 			}
-	// 		}
-	// 		var children []*process.Process
-	// 		if children, ee = proc.Children(); ee == nil {
-	// 			for _, child := range children {
-	// 				if childName, childErr := child.Name(); childErr == nil {
-	// 					if childName = filepath.Base(childName); childName == s.App.BinName {
-	// 						proc = child
-	// 						return
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
-	return
-}
-
-func (s *Slug) PrepareStart(port int) (webCmd string, webArgv, environ []string, err error) {
-
-	if ready := s.IsReady(); ready && port == s.Port {
-		err = fmt.Errorf("slug already running on given port: %v (PORT=%d)", s.Name, s.Port)
-		return
-	}
-
-	if common.IsAddressPortOpen(s.App.Origin.Host, port) {
-		err = fmt.Errorf("port already open by another process")
-		s.App.LogErrorF("%v: %d\n", err, port)
-		return
-	}
-	s.Port = port
-
-	var web string
-	if web, err = s.ReadProcfile(); err != nil {
-		err = fmt.Errorf("error reading Procfile: %v", err)
-		return
-	}
-
-	s.App.LogInfoF("preparing slug: PORT=%d %v (%v)\n", port, web, s.Name)
-
-	environ = append(s.App.OsEnviron(), fmt.Sprintf("PORT=%d", port))
-	var parsedArgs []string
-	if parsedArgs, err = common.ParseControlArgv(web); err != nil {
-		err = fmt.Errorf("error parsing Procfile web entry argv: %v \"%v\"", s.Name, web)
-		return
-	}
-	switch len(parsedArgs) {
-	case 0:
-		err = fmt.Errorf("error parsing Procfile web arguments: \"%v\"", web)
-	case 1:
-		webCmd = parsedArgs[0]
-	default:
-		webCmd = parsedArgs[0]
-		webArgv = parsedArgs[1:]
-	}
-	if found, _ := exec.LookPath(webCmd); found != "" {
-		webCmd = found
-	}
-
-	// s.App.LogInfoF("%v using log file: %v\n", s.App.Name, logfile)
-	// s.App.LogInfoF("%v using pid file: %v\n", s.App.Name, s.PidFile)
-	// s.App.LogInfoF("%v environment: %v\n", s.App.Name, environ)
-	return
-}
-
-func (s *Slug) StartForeground(port int) (err error) {
-	if err = s.Unpack(); err != nil {
-		err = fmt.Errorf("error unpacking this slug: %v - %v", s.Name, err)
-		return
-	}
-	var webCmd string
-	var webArgv, environ []string
-	if webCmd, webArgv, environ, err = s.PrepareStart(port); err != nil {
-		if strings.Contains(err.Error(), "slug already running") || strings.Contains(err.Error(), "maintenance mode") {
-			s.App.LogInfoF("%v", err)
-			err = nil
-			return
-		}
-		s.App.LogErrorF("error preparing slug: %v (port=%d) - %v\n", s.Name, port, err)
-		return
-	}
-
-	s.App.LogInfoF("starting slug web process: %v - %v %v\n", s.App.Name, webCmd, webArgv)
-	if err = run.ExeWith(&run.Options{Path: s.RunPath, Name: webCmd, Argv: webArgv, Stdout: s.LogFile, Stderr: s.LogFile, Environ: environ, PidFile: s.PidFile}); err != nil {
-		if strings.Contains(err.Error(), "signal: terminated") {
-			err = nil
-		} else {
-			err = fmt.Errorf("error executing slug: %v %v - %v", webCmd, webArgv, err)
-		}
-	}
-	return
-}
-
-func (s *Slug) Start(port int) (err error) {
-	if err = s.Unpack(); err != nil {
-		err = fmt.Errorf("error unpacking this slug: %v - %v", s.Name, err)
-		return
-	}
-	var webCmd string
-	var webArgv, environ []string
-	if webCmd, webArgv, environ, err = s.PrepareStart(port); err != nil {
-		if strings.Contains(err.Error(), "slug already running") || strings.Contains(err.Error(), "maintenance mode") {
-			s.App.LogInfoF("%v", err)
-			err = nil
-			return
-		}
-		s.App.LogErrorF("error preparing slug: %v (port=%d) - %v\n", s.Name, port, err)
-		return
-	}
-
-	s.App.LogInfoF("backgrounding slug web process: %v - %v %v\n", s.App.Name, webCmd, webArgv)
-	if pid, ee := run.BackgroundWith(&run.Options{Path: s.RunPath, Name: webCmd, Argv: webArgv, Stdout: s.LogFile, Stderr: s.LogFile, Environ: environ}); ee != nil {
-		err = fmt.Errorf("error backgrounding slug: %v %v - %v", webCmd, webArgv, ee)
-		return
-	} else {
-		if err = os.WriteFile(s.PidFile, []byte(strconv.Itoa(pid)), 0660); err != nil {
-			err = fmt.Errorf("error writing pidfile: %v - %v", s.PidFile, err)
-			return
-		} else {
-			s.App.LogInfoF("started process %d: %v on port %d\n", pid, s.Name, s.Port)
 		}
 	}
 
-	return
-}
-
-func (s *Slug) Stop() (stopped bool) {
-	if bePath.IsFile(s.PidFile) {
-		if proc, err := s.GetBinProcess(); err == nil && proc != nil {
-			if err = proc.SendSignal(syscall.SIGTERM); err != nil {
-				s.App.LogErrorF("error sending SIGTERM to process: %d\n", proc.Pid)
-			} else if stopped = err == nil; stopped {
-				s.App.LogInfoF("sent SIGTERM to process %d: %v\n", proc.Pid, s.Name)
-			}
-		} else if err != nil {
-			s.App.LogErrorF("error getting process from pid file: %v - %v\n", s.PidFile, err)
+	for _, hash := range maps.SortedKeys(hashes) {
+		// s.App.LogInfoF("loading slug instance with hash: %v.%v", s.Name, hash)
+		if si, err := NewSlugInstanceWithHash(s, hash); err == nil {
+			s.Instances = append(s.Instances, si)
 		}
 	}
-	if bePath.IsDir(s.RunPath) {
-		if err := os.RemoveAll(s.RunPath); err != nil {
-			s.App.LogErrorF("error removing slug run path: %v - %v\n", s.RunPath, err)
-		} else {
-			s.App.LogInfoF("removed slug run path: %v\n", s.RunPath)
-		}
-	}
-	if bePath.IsFile(s.PidFile) {
-		if err := os.Remove(s.PidFile); err != nil {
-			s.App.LogErrorF("error removing slug pid file: %v - %v\n", s.PidFile, err)
-		} else {
-			s.App.LogInfoF("removed slug pid file: %v\n", s.PidFile)
-		}
-	}
-	return
-}
-
-func (s *Slug) Destroy() (err error) {
-	s.Stop()
-	if bePath.IsDir(s.RunPath) {
-		if err = os.RemoveAll(s.RunPath); err != nil {
-			return
-		}
-	}
-	if bePath.IsFile(s.PidFile) {
-		if err = os.Remove(s.PidFile); err != nil {
-			return
-		}
-	}
-	if bePath.IsFile(s.Archive) {
-		if err = os.Remove(s.Archive); err != nil {
-			return
-		}
-	}
-	return
 }
 
 func (s *Slug) GetSlugStartupTimeout() (timeout time.Duration) {
@@ -371,5 +119,132 @@ func (s *Slug) GetReadyIntervalTimeout() (timeout time.Duration) {
 	default:
 		timeout = DefaultReadyIntervalTimeout
 	}
+	return
+}
+
+func (s *Slug) NumInstances() (num int) {
+	s.RLock()
+	defer s.RUnlock()
+	num = len(s.Instances)
+	return
+}
+
+func (s *Slug) GetPort(idx int) (port int) {
+	s.RLock()
+	defer s.RUnlock()
+	if num := len(s.Instances); num > 0 && idx < num {
+		port = s.Instances[idx].Port
+	}
+	return
+}
+
+func (s *Slug) IsRunning(idx int) (running bool) {
+	s.RLock()
+	defer s.RUnlock()
+	if num := len(s.Instances); num > 0 && idx < num {
+		running = s.Instances[idx].IsRunning()
+	}
+	return
+}
+
+func (s *Slug) IsReady(idx int) (ready bool) {
+	s.RLock()
+	defer s.RUnlock()
+	if num := len(s.Instances); num > 0 && idx < num {
+		ready = s.Instances[idx].IsReady()
+	}
+	return
+}
+
+func (s *Slug) IsRunningReady(idx int) (running, ready bool) {
+	s.RLock()
+	defer s.RUnlock()
+	if num := len(s.Instances); num > 0 && idx < num {
+		running, ready = s.Instances[idx].IsRunningReady()
+	}
+	return
+}
+
+func (s *Slug) Stop(idx int) (stopped bool) {
+	s.Lock()
+	defer s.Unlock()
+	if num := len(s.Instances); num > 0 && idx < num {
+		stopped = s.Instances[idx].Stop()
+		if last := len(s.Instances) - 1; idx < last {
+			s.Instances = append(s.Instances[:idx], s.Instances[idx+1:]...)
+		} else {
+			s.Instances = s.Instances[:idx]
+		}
+	}
+	return
+}
+
+func (s *Slug) StopAll() (stopped int) {
+	s.Lock()
+	defer s.Unlock()
+	for _, si := range s.Instances {
+		if si.Stop() {
+			stopped += 1
+		}
+	}
+	s.Instances = make([]*SlugInstance, 0)
+	return
+}
+
+func (s *Slug) SendSignal(idx int, signal process.Signal) {
+	s.Lock()
+	defer s.Unlock()
+	if num := len(s.Instances); num > 0 && idx < num {
+		if proc, err := s.Instances[idx].GetBinProcess(); err == nil && proc != nil {
+			_ = proc.SendSignal(signal)
+		}
+	}
+}
+
+func (s *Slug) SendSignalToAll(signal process.Signal) {
+	s.Lock()
+	defer s.Unlock()
+	for _, si := range s.Instances {
+		if proc, err := si.GetBinProcess(); err == nil && proc != nil {
+			_ = proc.SendSignal(signal)
+		}
+	}
+}
+
+func (s *Slug) Destroy() (err error) {
+	s.Lock()
+	defer s.Unlock()
+	for _, si := range s.Instances {
+		si.Stop()
+	}
+	if bePath.IsFile(s.Archive) {
+		if err = os.Remove(s.Archive); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (s *Slug) StartForeground(port int) (err error) {
+	var si *SlugInstance
+	if si, err = NewSlugInstance(s); err != nil {
+		return
+	}
+	s.Lock()
+	s.Instances = append(s.Instances, si)
+	s.Unlock()
+	err = si.StartForeground(port)
+	return
+}
+
+func (s *Slug) Start(port int) (err error) {
+	var si *SlugInstance
+	if si, err = NewSlugInstance(s); err != nil {
+		return
+	}
+	s.Lock()
+	s.Instances = append(s.Instances, si)
+	s.Unlock()
+	err = si.Start(port)
 	return
 }
