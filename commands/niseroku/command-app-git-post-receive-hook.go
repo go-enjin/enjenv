3b57e76626f0429b4c0a585859f4a1734b823c8b
Kevin C. Krinke <kevin@krinke.ca>
2023-03-05 14:46:05 -0500

    commands/niseroku (git post-receive hook): refactored to better handle apt-package building and code re-use; updated for latest buildpack detection changes; only signal git-repository for reload during apt-package deployments

--- commands/niseroku/command-app-git-post-receive-hook.go
+++ commands/niseroku/command-app-git-post-receive-hook.go
@@ -18,9 +18,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/dustin/go-humanize"
+	"github.com/go-enjin/be/pkg/cli/env"
 	"github.com/go-git/go-git/v5"
 	cp "github.com/otiai10/copy"
 	"github.com/sosedoff/gitkit"
@@ -30,6 +32,7 @@ import (
 	bePath "github.com/go-enjin/be/pkg/path"
 	beStrings "github.com/go-enjin/be/pkg/strings"
 
+	"github.com/go-enjin/enjenv/pkg/globals"
 	pkgIo "github.com/go-enjin/enjenv/pkg/io"
 	pkgRun "github.com/go-enjin/enjenv/pkg/run"
 	"github.com/go-enjin/enjenv/pkg/service/common"
@@ -50,7 +53,7 @@ func (c *Command) actionAppGitPostReceiveHook(ctx *cli.Context) (err error) {
 			if app, err = c.enjinRepoGitHandlerSetup(c.config, info); err != nil {
 				return
 			}
-			err = c.enjinRepoPostReceiveHandler(app, c.config, info, tmpPath) // your handler function
+			err = c.enjinRepoPostReceiveHandler(app, c.config, info, tmpPath)
 			return
 		},
 	}
@@ -61,194 +64,85 @@ func (c *Command) actionAppGitPostReceiveHook(ctx *cli.Context) (err error) {
 
 func (c *Command) enjinRepoPostReceiveHandler(app *Application, config *Config, info *gitkit.HookInfo, tmpPath string) (err error) {
 
-	if info.RefName == "main" {
-		err = c.enjinRepoRunBuildpackProcess(app, config, info.NewRev, tmpPath)
+	var bpi buildPackInfo
+	if bpi, err = c.enjinRepoPrepareBuildpackProcess(app, config, info, tmpPath); err != nil {
 		return
 	}
 
-	var ok bool
-	var aptApp *Application
-	var ap *AptPackageConfig
-	var ae *AptEnjinConfig
-	var dists []Distribution
-
-	if ap = app.AptPackage; ap == nil {
-		err = fmt.Errorf("unsupported branch received: %v", info.RefName)
-		return
-	} else if aptApp, ok = c.config.Applications[ap.AptEnjin]; !ok {
-		err = fmt.Errorf("apt-package.apt-enjin not found: %v", ap.AptEnjin)
-		return
-	} else if ae = aptApp.AptEnjin; ae == nil {
-		err = fmt.Errorf("apt-package.apt-enjin not not an apt-enjin: %v", aptApp.Name)
-		return
-	} else if !ae.Enable {
-		err = fmt.Errorf("apt-enjin not enabled: %v", aptApp.Name)
-		return
-	} else if dists, ok = ae.Flavours[info.RefName]; !ok {
-		err = fmt.Errorf("apt-enjin flavour not supported: %v", info.RefName)
-		return
-	}
-
-	var distribution *Distribution
-	for _, dist := range dists {
-		if dist.Codename == ap.Codename {
-			distribution = &dist
-			break
-		}
-	}
-	if distribution == nil {
-		err = fmt.Errorf("apt-package.codename not supported by apt-enjin: %v - %v", ap.Codename, aptApp.Name)
-		return
-	}
-
-	tmpName := bePath.Base(tmpPath)
-	buildDir := config.Paths.TmpBuild + "/" + tmpName
-
-	pkgIo.STDOUT("# preparing BUILD_DIR...\n")
-	if err = cp.Copy(tmpPath, buildDir); err != nil {
-		err = fmt.Errorf("error copying to enjin build path: %v - %v", buildDir, err)
-		return
-	}
 	defer func() {
-		// cleanup build dir, if success, zip is all that is needed
-		pkgIo.STDOUT("# cleaning BUILD_DIR...\n")
-		_ = os.RemoveAll(buildDir)
+		if ee := c.enjinRepoCleanupBuildpackProcess(app, config, info.NewRev, tmpPath); ee != nil {
+			pkgIo.STDERR("error cleaning up buildpack process: %v - %v", app.Name, ee)
+		}
 	}()
 
-	pwd := bePath.Pwd()
-	if err = os.Chdir(buildDir); err != nil {
-		err = fmt.Errorf("chdir error: %v - %v", buildDir, err)
+	switch bpi.detected {
+
+	case "enjin-slug":
+		err = c.enjinRepoBuildEnjinSlug(bpi)
 		return
-	}
-	defer func() {
-		_ = os.Chdir(pwd)
-	}()
 
-	if !bePath.IsFile("Procfile") {
-		err = fmt.Errorf("application Procfile not found: %v - %v/Procfile", app.Name, buildDir)
+	case "apt-package":
+		err = c.enjinRepoBuildAptPackage(bpi)
 		return
+
 	}
 
-	var procTypes map[string]string
-	if procTypes, err = common.ReadProcfile("Procfile"); err != nil {
-		err = fmt.Errorf("error reading Procfile: %v - %v", app.Name, err)
-		return
-	}
-
-	var commandline string
-	if commandline, ok = procTypes[info.RefName]; !ok {
-		err = fmt.Errorf("application Procfile does not support %v: %v", info.RefName, app.Name)
-		return
-	}
-
-	var name string
-	var argv []string
-	parts := strings.Split(commandline, " ")
-	if numParts := len(parts); numParts == 0 {
-		err = fmt.Errorf("application Procfile %v target command empty: %v", info.RefName, app.Name)
-		return
-	} else if numParts > 1 {
-		name = parts[0]
-		argv = parts[1:]
-	} else {
-		name = parts[0]
-	}
-
-	var gpgInfo map[string][]string
-	if gpgInfo, err = app.ImportGpgSecrets(aptApp); err != nil {
-		err = fmt.Errorf("error preparing gpg secrets: %v - %v", aptApp.Name, err)
-		return
-	}
-
-	var signWith string
-	for _, fingerprints := range gpgInfo {
-		if beStrings.StringInSlices(distribution.SignWith, fingerprints) {
-			signWith = distribution.SignWith
-			break
-		}
-	}
-
-	if signWith == "" {
-		err = fmt.Errorf("distribution signing key not found: %v [%v] - %v", aptApp.Name, distribution.Codename, distribution.SignWith)
-		return
-	}
-
-	gpgHome := app.GetGpgHome()
-	if !bePath.Exists(gpgHome) {
-		if err = os.MkdirAll(gpgHome, 0700); err != nil {
-			err = fmt.Errorf("error making gpg home: %v - %v", app.Name, err)
-			return
-		}
-	}
-
-	appOsEnviron := append(
-		app.OsEnviron(),
-		"GNUPGHOME="+gpgHome,
-		"AE_GPG_HOME="+gpgHome,
-		"AE_SIGN_KEY="+signWith,
-		"AE_ARCHIVES="+aptApp.AptArchivesPath,
-		"UNTAGGED_COMMIT="+info.NewRev[:10],
-	)
-
-	pkgIo.STDOUT("# starting %v build process: %v - %v\n", info.RefName, name, argv)
-
-	if err = run.ExeWith(&run.Options{Path: ".", Name: name, Argv: argv, Environ: appOsEnviron}); err != nil {
-		return
-	}
-
-	pkgIo.STDOUT("# signaling for niseroku service reload\n")
-	c.config.SignalReloadGitRepository()
-	c.config.SignalReloadReverseProxy()
+	err = fmt.Errorf("unsupported buildpack deployment: %+v", bpi)
 	return
 }
 
-func (c *Command) enjinRepoRunBuildpackProcess(app *Application, config *Config, commitId, tmpPath string) (err error) {
+type buildPackInfo struct {
+	app      *Application
+	config   *Config
+	info     *gitkit.HookInfo
+	tmpPath  string
+	tmpName  string
+	buildDir string
+	cacheDir string
+	cloneDir string
+	envDir   string
+	detected string
+}
 
-	tmpName := bePath.Base(tmpPath)
-	buildDir := config.Paths.TmpBuild + "/" + tmpName
-	cacheDir := config.Paths.VarCache + "/" + app.Name
-	slugZip := config.Paths.VarSlugs + "/" + app.Name + "--" + commitId + ".zip"
-	buildPackClonePath := config.Paths.TmpClone + "/" + app.Name
-	envDir := config.Paths.VarSettings + "/" + app.Name
+func (c *Command) enjinRepoPrepareBuildpackProcess(app *Application, config *Config, info *gitkit.HookInfo, tmpPath string) (bpi buildPackInfo, err error) {
+
+	bpi = buildPackInfo{
+		app:     app,
+		config:  config,
+		info:    info,
+		tmpPath: tmpPath,
+	}
+	bpi.tmpName = bePath.Base(tmpPath)
+	bpi.buildDir = config.Paths.TmpBuild + "/" + bpi.tmpName
+	bpi.cacheDir = config.Paths.VarCache + "/" + app.Name
+	bpi.cloneDir = config.Paths.TmpClone + "/" + app.Name
+	bpi.envDir = config.Paths.VarSettings + "/" + app.Name
 
 	pkgIo.STDOUT("# preparing ENV_DIR...\n")
-	if bePath.IsDir(envDir) {
-		if err = os.RemoveAll(envDir); err != nil {
-			err = fmt.Errorf("error removing enjin env path: %v - %v", envDir, err)
+	if bePath.IsDir(bpi.envDir) {
+		if err = os.RemoveAll(bpi.envDir); err != nil {
+			err = fmt.Errorf("error removing enjin env path: %v - %v", bpi.envDir, err)
 			return
 		}
 	}
-	if err = bePath.Mkdir(envDir); err != nil {
-		err = fmt.Errorf("error making enjin deployment path: %v - %v", envDir, err)
-		return
-	}
-	defer func() {
-		pkgIo.STDOUT("# cleaning ENV_DIR...\n")
-		_ = os.RemoveAll(envDir)
-	}()
-	if err = app.ApplySettings(envDir); err != nil {
-		err = fmt.Errorf("error applying enjin settings: %v - %v", envDir, err)
+	if err = bePath.Mkdir(bpi.envDir); err != nil {
+		err = fmt.Errorf("error making enjin deployment path: %v - %v", bpi.envDir, err)
 		return
 	}
 
 	pkgIo.STDOUT("# preparing CACHE_DIR...\n")
-	if !bePath.IsDir(cacheDir) {
-		if err = bePath.Mkdir(cacheDir); err != nil {
-			err = fmt.Errorf("error making enjin deployment path: %v - %v", cacheDir, err)
+	if !bePath.IsDir(bpi.cacheDir) {
+		if err = bePath.Mkdir(bpi.cacheDir); err != nil {
+			err = fmt.Errorf("error making enjin deployment path: %v - %v", bpi.cacheDir, err)
 			return
 		}
 	}
 
 	pkgIo.STDOUT("# preparing BUILD_DIR...\n")
-	if err = cp.Copy(tmpPath, buildDir); err != nil {
-		err = fmt.Errorf("error copying to enjin build path: %v - %v", buildDir, err)
+	if err = cp.Copy(tmpPath, bpi.buildDir); err != nil {
+		err = fmt.Errorf("error copying to enjin build path: %v - %v", bpi.buildDir, err)
 		return
 	}
-	defer func() {
-		// cleanup build dir, if success, zip is all that is needed
-		pkgIo.STDOUT("# cleaning BUILD_DIR...\n")
-		_ = os.RemoveAll(buildDir)
-	}()
 
 	pkgIo.STDOUT("# preparing enjenv buildpack...\n")
 	var buildPack string
@@ -258,38 +152,69 @@ func (c *Command) enjinRepoRunBuildpackProcess(app *Application, config *Config,
 		buildPack = DefaultBuildPack
 	}
 	if bePath.IsDir(config.BuildPack) {
-		if err = cp.Copy(config.BuildPack, buildPackClonePath); err != nil {
+		if err = cp.Copy(config.BuildPack, bpi.cloneDir); err != nil {
 			return
 		}
-	} else if _, err = git.PlainClone(buildPackClonePath, false, &git.CloneOptions{URL: buildPack}); err != nil {
+	} else if _, err = git.PlainClone(bpi.cloneDir, false, &git.CloneOptions{URL: buildPack}); err != nil {
 		return
 	}
-	defer func() {
-		pkgIo.STDOUT("# cleaning enjenv buildpack...\n")
-		_ = os.RemoveAll(buildPackClonePath)
-	}()
+
+	pkgIo.STDOUT("# buildpack: detecting...\n")
 
 	var status int
-	pkgIo.STDOUT("# buildpack: detected... ")
-	if status, err = run.Exe(buildPackClonePath+"/bin/detect", buildDir); err != nil {
-		err = fmt.Errorf("error detecting buildpack: %v", err)
-		pkgIo.STDOUT("\n")
+	var o string
+	if o, _, status, err = run.Cmd(bpi.cloneDir+"/bin/detect", bpi.buildDir); err != nil {
+		err = fmt.Errorf("error running buildpack detection: %v", err)
 		return
-	} else if status != 0 {
-		pkgIo.STDOUT("\n")
+	} else if status > 0 {
+		err = fmt.Errorf("buildpack exited with non-zero status: %d", status)
+		return
+	} else if bpi.detected = strings.TrimSpace(o); bpi.detected == "" {
+		err = fmt.Errorf("buildpack did not detect any enjin")
 		return
 	}
 
-	if ae := app.AptEnjin; ae != nil {
-		procfile := filepath.Join(buildDir, "Procfile")
+	pkgIo.STDOUT("# buildpack: detected: %v\n", bpi.detected)
+
+	return
+}
+
+func (c *Command) enjinRepoCleanupBuildpackProcess(app *Application, config *Config, commitId, tmpPath string) (err error) {
+
+	tmpName := bePath.Base(tmpPath)
+	buildDir := config.Paths.TmpBuild + "/" + tmpName
+	buildPackClonePath := config.Paths.TmpClone + "/" + app.Name
+	envDir := config.Paths.VarSettings + "/" + app.Name
+
+	pkgIo.STDOUT("# cleaning ENV_DIR...\n")
+	_ = os.RemoveAll(envDir)
+	pkgIo.STDOUT("# cleaning BUILD_DIR...\n")
+	_ = os.RemoveAll(buildDir)
+	pkgIo.STDOUT("# cleaning enjenv buildpack...\n")
+	_ = os.RemoveAll(buildPackClonePath)
+	return
+}
+
+func (c *Command) enjinRepoBuildEnjinSlug(bpi buildPackInfo) (err error) {
+
+	if bpi.info.RefName != "main" {
+		err = fmt.Errorf("unsupported enjin slug target branch: %v", bpi.info.RefName)
+		return
+	}
+
+	commitId := bpi.info.NewRev
+	slugZip := bpi.config.Paths.VarSlugs + "/" + bpi.app.Name + "--" + commitId + ".zip"
+
+	if ae := bpi.app.AptEnjin; ae != nil {
+		procfile := filepath.Join(bpi.buildDir, "Procfile")
 		if bePath.IsFile(procfile) {
 			if procTypes, ee := common.ReadProcfile(procfile); ee != nil {
 				pkgIo.STDERR("apt-enjin Procfile error: %v\n", ee)
 			} else {
-				if eee := app.PrepareGpgSecrets(); eee != nil {
+				if eee := bpi.app.PrepareGpgSecrets(); eee != nil {
 					pkgIo.STDERR("apt-enjin prepare gpg error: %v\n", eee)
 				}
-				osEnviron := app.OsEnviron()
+				osEnviron := bpi.app.OsEnviron()
 				for flavour, _ := range ae.Flavours {
 					if command, ok := procTypes[flavour]; ok {
 						pkgIo.STDOUT("# apt-enjin: detected Procfile target - %v\n", flavour)
@@ -303,7 +228,7 @@ func (c *Command) enjinRepoRunBuildpackProcess(app *Application, config *Config,
 							}
 						}
 						if eee := run.ExeWith(&run.Options{
-							Path:    buildDir,
+							Path:    bpi.buildDir,
 							Name:    name,
 							Argv:    args,
 							Environ: osEnviron,
@@ -318,15 +243,16 @@ func (c *Command) enjinRepoRunBuildpackProcess(app *Application, config *Config,
 		}
 	}
 
+	var status int
 	pkgIo.STDOUT("# buildpack: compiling...\n")
-	if status, err = run.Exe(buildPackClonePath+"/bin/compile", buildDir, cacheDir, envDir); err != nil {
+	if status, err = run.Exe(bpi.cloneDir+"/bin/compile", bpi.buildDir, bpi.cacheDir, bpi.envDir); err != nil {
 		return
 	} else if status != 0 {
 		return
 	}
 
 	pwd := bePath.Pwd()
-	if err = os.Chdir(buildDir); err != nil {
+	if err = os.Chdir(bpi.buildDir); err != nil {
 		return
 	}
 
@@ -350,5 +276,157 @@ func (c *Command) enjinRepoRunBuildpackProcess(app *Application, config *Config,
 
 	err = pkgRun.EnjenvExe("niseroku", "deploy-slug", slugZip)
 
+	return
+}
+
+var RxExportLine = regexp.MustCompile(`^\s*export (.+?)=(['"]?)(.+?)(['"]?)\s*$`)
+
+func (c *Command) enjinRepoBuildAptPackage(bpi buildPackInfo) (err error) {
+
+	var ok bool
+	var aptApp *Application
+	var ap *AptPackageConfig
+	var ae *AptEnjinConfig
+	var dists []Distribution
+
+	if ap = bpi.app.AptPackage; ap == nil {
+		err = fmt.Errorf("unsupported branch received: %v", bpi.info.RefName)
+		return
+	} else if aptApp, ok = c.config.Applications[ap.AptEnjin]; !ok {
+		err = fmt.Errorf("apt-package.apt-enjin not found: %v", ap.AptEnjin)
+		return
+	} else if ae = aptApp.AptEnjin; ae == nil {
+		err = fmt.Errorf("apt-package.apt-enjin not not an apt-enjin: %v", aptApp.Name)
+		return
+	} else if !ae.Enable {
+		err = fmt.Errorf("apt-enjin not enabled: %v", aptApp.Name)
+		return
+	} else if dists, ok = ae.Flavours[bpi.info.RefName]; !ok {
+		err = fmt.Errorf("apt-enjin flavour not supported: %v", bpi.info.RefName)
+		return
+	}
+
+	var distribution *Distribution
+	for _, dist := range dists {
+		if dist.Codename == ap.Codename {
+			distribution = &dist
+			break
+		}
+	}
+	if distribution == nil {
+		err = fmt.Errorf("apt-package.codename not supported by apt-enjin: %v - %v", ap.Codename, aptApp.Name)
+		return
+	}
+
+	osEnviron := bpi.app.OsEnviron()
+	enjenvPath := bpi.cacheDir
+	enjInitEnviron := append(osEnviron, "ENJENV_PATH="+enjenvPath)
+
+	enjenvInitGolangArgv := []string{"init", "--force", "--golang", env.Get("ENJENV_BUILDPACK_GOLANG", globals.DefaultGolangVersion)}
+	if err = pkgRun.EnjenvExeWith(bpi.buildDir, enjInitEnviron, enjenvInitGolangArgv...); err != nil {
+		err = pkgIo.ErrorF("enjenv golang init error: %v", err)
+		return
+	}
+
+	var exportOutput string
+	if exportOutput, _, err = pkgRun.EnjenvCmdWith(bpi.buildDir, enjInitEnviron, "export"); err != nil {
+		err = pkgIo.ErrorF("enjenv export env error: %v", err)
+		return
+	}
+
+	pkgIo.STDOUT("export output:\n%v\n", exportOutput)
+
+	enjEnviron := osEnviron
+	for _, line := range strings.Split(exportOutput, "\n") {
+		if RxExportLine.MatchString(line) {
+			m := RxExportLine.FindAllStringSubmatch(line, 1)
+			k, v := m[0][1], m[0][3]
+			enjEnviron = append(enjEnviron, k+"="+v)
+		}
+	}
+
+	pwd := bePath.Pwd()
+	if err = os.Chdir(bpi.buildDir); err != nil {
+		err = fmt.Errorf("chdir error: %v - %v", bpi.buildDir, err)
+		return
+	}
+	defer func() {
+		_ = os.Chdir(pwd)
+	}()
+
+	if !bePath.IsFile("Procfile") {
+		err = fmt.Errorf("application Procfile not found: %v - %v/Procfile", bpi.app.Name, bpi.buildDir)
+		return
+	}
+
+	var procTypes map[string]string
+	if procTypes, err = common.ReadProcfile("Procfile"); err != nil {
+		err = fmt.Errorf("error reading Procfile: %v - %v", bpi.app.Name, err)
+		return
+	}
+
+	var commandline string
+	if commandline, ok = procTypes[bpi.info.RefName]; !ok {
+		err = fmt.Errorf("application Procfile does not support %v: %v", bpi.info.RefName, bpi.app.Name)
+		return
+	}
+
+	var name string
+	var makeFlavourArgv []string
+	parts := strings.Split(commandline, " ")
+	if numParts := len(parts); numParts == 0 {
+		err = fmt.Errorf("application Procfile %v target command empty: %v", bpi.info.RefName, bpi.app.Name)
+		return
+	} else if numParts > 1 {
+		name = parts[0]
+		makeFlavourArgv = parts[1:]
+	} else {
+		name = parts[0]
+	}
+
+	var gpgInfo map[string][]string
+	if gpgInfo, err = bpi.app.ImportGpgSecrets(aptApp); err != nil {
+		err = fmt.Errorf("error preparing gpg secrets: %v - %v", aptApp.Name, err)
+		return
+	}
+
+	var signWith string
+	for _, fingerprints := range gpgInfo {
+		if beStrings.StringInSlices(distribution.SignWith, fingerprints) {
+			signWith = distribution.SignWith
+			break
+		}
+	}
+
+	if signWith == "" {
+		err = fmt.Errorf("distribution signing key not found: %v [%v] - %v", aptApp.Name, distribution.Codename, distribution.SignWith)
+		return
+	}
+
+	gpgHome := bpi.app.GetGpgHome()
+	if !bePath.Exists(gpgHome) {
+		if err = os.MkdirAll(gpgHome, 0700); err != nil {
+			err = fmt.Errorf("error making gpg home: %v - %v", bpi.app.Name, err)
+			return
+		}
+	}
+
+	appOsEnviron := append(
+		enjEnviron,
+		"GNUPGHOME="+gpgHome,
+		"AE_GPG_HOME="+gpgHome,
+		"AE_SIGN_KEY="+signWith,
+		"AE_ARCHIVES="+aptApp.AptArchivesPath,
+		"UNTAGGED_COMMIT="+bpi.info.NewRev[:10],
+	)
+
+	pkgIo.STDOUT("# starting %v build process: %v - %v\n", bpi.info.RefName, name, makeFlavourArgv)
+
+	if err = run.ExeWith(&run.Options{Path: ".", Name: name, Argv: makeFlavourArgv, Environ: appOsEnviron}); err != nil {
+		return
+	}
+
+	pkgIo.STDOUT("# signaling niseroku git-repository reload\n")
+	c.config.SignalReloadGitRepository()
 	return
 }
