108698e74838e429b32e6d1e89c32aa3b97d299e
Kevin C. Krinke <kevin@krinke.ca>
2023-08-28 14:24:25 -0400

    Golang.cmd.mk: bugfix for be-update target

--- Golang.cmd.mk
+++ Golang.cmd.mk
@@ -286,4 +286,4 @@ __be_update: PKG_LIST=$(call __pkg_list_latest)
 __be_update: __golang
 	@$(call __validate_extra_pkgs)
 	@echo "# go getting: ${PKG_LIST}"
-	@GOPROXY=direct $(call __go_bin) get ${_BUILD_TAGS} ${PKG_LIST}
+	@GOPROXY=direct $(call __go_bin) get ${PKG_LIST}
